54fa3c863395b67e840e58f2b2860e9fd486bb5a
shangxiangpeng <524110886@qq.com>
2017-12-07 16:00:09 +0800

    案例列表页

--- js/main.js
+++ js/main.js
@@ -1,3 +1,16 @@
-$(function() {
+$(function () {
+    var $controlBar = $('#control-bar');
+    var $fixedBarWrap = $('#fixed-bar-wrap');
+    $controlBar.click(function () {
+        var bottomVal = $fixedBarWrap.css('bottom');
+        console.log(bottomVal === '-372px')
+        if (bottomVal === '-372px') {
+            $fixedBarWrap.stop(true, true).animate({'bottom': 0});
+        } else {
+            $fixedBarWrap.stop(true, true).animate({'bottom': '-372px'});
+        }
+    })
+
+
 
 });
